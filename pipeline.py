366bcf86e0015380714b5950148673ae11fa99d5
matthewgalloway <matthew.galloway19@gmail.com>
2020-10-31 17:38:18 +0000

    adding vis pipeline

--- pipeline.py
+++ pipeline.py
@@ -34,3 +34,29 @@ income_pipeline = Pipeline(
         ),
     ]
         )
+
+
+# #%% Modelling
+#
+#
+#
+# X_train_processed =income_pipeline.fit_transform(
+#                                                     X_train[config.FEATURES], y_train)
+#
+# X_test_processed = income_pipeline.fit_transform(
+#                                                     X_test[config.FEATURES], y_test)
+#
+# #%% Modeling
+#
+# corr_matric = X_train_processed.corr()
+#
+# corr_matric[(1>corr_matric)&(corr_matric>0.6)]
+#
+# #%% Modeling
+#
+# model = RandomForestClassifier(random_state=0)
+# model.fit(X_train_processed, y_train)
+# predicted_classes = model.predict(X_test_processed)
+# print(accuracy_score(predicted_classes, y_test.values))
+# print(confusion_matrix(predicted_classes, y_test.values))
+# print(f1_score(predicted_classes, y_test.values))
